c346dfdd4bb57f6e89e599ae0048fd8f114b9b07
Shreyas <shreyaskulkarni358@gmail.com>
2023-05-23 10:40:15 +0530

    day_4

---
+++ Assignment/src/Pisyst/Arrray_3.java
@@ -0,0 +1,15 @@
+package Pisyst;
+
+public class Arrray_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubi
+		int [] marks = {10,20,30,40,50,60};
+		float [] mar = {98.1f,25.6f,22.5f,52.6f,22.9f,};
+		System.out.println(marks.length);
+		System.out.println(mar[2]);
+		
+
+	}
+
+}
