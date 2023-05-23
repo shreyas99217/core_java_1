b7360ed19046d7b54902dbfbec518dbeceb3261a
Shreyas <shreyaskulkarni358@gmail.com>
2023-05-23 17:34:19 +0530

    Day__4

---
+++ Assignment/src/Pisyst/Array_4.java
@@ -0,0 +1,16 @@
+package Pisyst;
+
+public class Array_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int [] a= {10,20,30,40,50,60};
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+
+		
+	}
+
+}
---
+++ Assignment/src/Pisyst/String1.java
@@ -0,0 +1,16 @@
+package Pisyst;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1,s2;
+		s1="Hello";
+		s2="Hello";
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+	}
+
+}
